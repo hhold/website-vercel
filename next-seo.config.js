50e349135011aa2d442c45b99a003ca105e8bc30
Hitesh Nair <htshnr@gmail.com>
2021-09-10 00:33:07 +0530

    yeah

---
+++ next-seo.config.js
@@ -0,0 +1,12 @@
+export default {
+    openGraph: {
+      type: 'website',
+      url: 'https://www.hiteshnair.com/',
+      site_name: 'Hitesh Nair',
+    },
+    twitter: {
+      handle: '@htshnr',
+      cardType: 'summary_large_image',
+    },
+  };
+  
