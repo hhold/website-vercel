efe32f43698150b88435dc6d7a4934a09962cd8e
Hitesh Nair <10278525+hhold@users.noreply.github.com>
2021-09-22 00:56:58 +0530

    stats

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 // import 'tailwindcss/tailwind.css'
+import Script from 'next/script'
 import "../styles/globals.css";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
@@ -6,6 +7,38 @@ import SEO from "../next-seo.config";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+          <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+        `}
+      </Script>
+
+      <Script>
+        {` window._qevents = window._qevents || [];
+    
+    (function() {
+    var elem = document.createElement('script');
+    elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+    elem.async = true;
+    elem.type = "text/javascript";
+    var scpt = document.getElementsByTagName('script')[0];
+    scpt.parentNode.insertBefore(elem, scpt);
+    })();
+    
+    window._qevents.push({
+    qacct:${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}
+    });`}
+        
+      </Script>
+
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
